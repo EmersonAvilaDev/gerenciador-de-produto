60f536fde640ac59a134d0f657fdcef8b650aeed
EmersonAvilaDev <avilaadsemerson@gmail.com>
2023-06-30 17:26:37 -0300

    add: function/list and delete

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ class Product {
     if (this.validate(product) == true) {
       this.save(product);
     }
-
-    console.log(this.arrProducts);
+    this.list();
+    this.cancel();
   }
 
   readData() {
@@ -46,6 +46,48 @@ class Product {
   save(product) {
     this.arrProducts.push(product);
     this.id++;
+  }
+
+  list() {
+    let tbody = document.getElementById("tbody");
+
+    tbody.innerHTML = "";
+
+    for (let i = 0; i < this.arrProducts.length; i++) {
+      let tr = tbody.insertRow();
+      let td_id = tr.insertCell();
+      let td_name = tr.insertCell();
+      let td_price = tr.insertCell();
+      let td_delete = tr.insertCell();
+
+      td_id.innerText = this.arrProducts[i].id;
+      td_name.innerText = this.arrProducts[i].nameProduct;
+      td_price.innerText = this.arrProducts[i].priceProduct;
+
+      let img = document.createElement("img");
+      img.src = "./images/del.png";
+      img.setAttribute(
+        "onclick",
+        "product.delete(" + this.arrProducts[i].id + ")"
+      );
+      td_delete.appendChild(img);
+    }
+  }
+
+  cancel() {
+    document.getElementById("pdName").value = "";
+    document.getElementById("pdPrice").value = "";
+  }
+
+  delete(id) {
+    let tbody = document.getElementById("tbody");
+    for (let i = 0; i < this.arrProducts.length; i++) {
+      if (this.arrProducts[i].id == id) {
+        this.arrProducts.splice(i, 1);
+        tbody.deleteRow(i);
+      }
+    }
+    alert("O item foi apagado com sucesso!");
   }
 }
 
